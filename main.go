5726e403f8129bf76d45634b0114f11f9730e395
ZhengjunHUO <firelouiszj@hotmail.com>
2022-05-19 16:55:08 +0200

    add POST ethod

--- main.go
+++ main.go
@@ -23,14 +23,36 @@ var clusters = []k8sCluster{
 func main() {
 	server := gin.Default()
 	// associate GET methods and path with a handler func
+	// do $ curl http://localhost:8088/clusters
 	server.GET("/clusters", getClusters)
+	/* do
+	$ curl http://localhost:8088/clusters \
+		--include \
+		--header "Content-Type: application/json" \
+		--request "POST" \
+		--data '{"id": "4","cni": "awsvpc","size": 6,"ismanaged": true, "isbaremetal": false, "isoverlay": true}'
+	*/
+	server.POST("/clusters", addCluster)
 
 	server.Run("localhost:8088")
 }
 
-// hander func
+// handler func to get a list of k8s cluster
 func getClusters(c *gin.Context) {
 	// Context carries request details and more ...
 	// marshaling the struct to json & add to response
 	c.IndentedJSON(http.StatusOK, clusters)
+}
+
+// handler func to add a k8s cluster to the pool
+func addCluster(c *gin.Context) {
+	var cluster k8sCluster
+
+	// Bind the request body(recieved json) in to a k8sCluster struct
+	if err := c.BindJSON(&cluster); err != nil {
+		return
+	}
+
+	clusters = append(clusters, cluster)
+	c.IndentedJSON(http.StatusCreated, cluster)
 }
